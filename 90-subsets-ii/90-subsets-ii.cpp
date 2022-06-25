b586073ea0f6967632ed57fb4700b452ca917516
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-06-25 11:07:05 +0530

    Time: 46 ms (5.45%), Space: 13.4 MB (25.13%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    void subset_sum(int index, vector<int>& nums, set<vector<int>>& sum, vector<int>&temp, int n){
+        if(index==n) {
+            if(std::find (sum.begin(), sum.end(), temp)== sum.end()) {
+            sum.insert(temp);    
+            }
+            
+            return ;
+        }
+        
+      temp.push_back(nums[index]);
+     //ans +=arr[index];
+      //cout<< ans<< " ";
+      
+      subset_sum(index+1,nums,sum,temp, n);
+      
+      temp.pop_back();
+    //   ans -=arr[index];
+      //cout<< ans<< " ";
+      
+      subset_sum(index+1,nums,sum,temp,n);
+       
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        set<vector<int>> sum;
+        int N= nums.size();
+        //int ans;
+        vector<int> temp;
+        subset_sum(0,nums,sum,temp,N);
+        std::vector<vector<int>> v(sum.begin(), sum.end());
+
+        return v;
+    }
+};
