12ae42511c383cfa916203c1c4e1ef453c6a0cd6
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-06-26 13:21:19 +0530

    Time: 4 ms (64.64%), Space: 7.6 MB (91.87%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    void recursion_permute(vector<int>&nums,vector<vector<int>>&ans, vector<int>&temp, int freq[]){
-        if(temp.size()==nums.size()){
-            ans.push_back(temp);
+    void recursion_permute(int index,vector<int>&nums,vector<vector<int>>&ans){
+        if(index==nums.size()){
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++){
-            if(!freq[i]){
-                freq[i]=1;
-                temp.push_back(nums[i]);
-                recursion_permute(nums,ans, temp, freq);
-                temp.pop_back();
-                freq[i]=0;
-                
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            recursion_permute(index+1,nums,ans);
+            swap(nums[index],nums[i]);
             }
-        }
+        
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> temp;
-        int freq[nums.size()];
-        for(int i=0;i<nums.size();i++) freq[i]=0;
-        recursion_permute(nums, ans, temp, freq);
+        // vector<int> temp;
+        // int freq[nums.size()];
+        // for(int i=0;i<nums.size();i++) freq[i]=0;
+        recursion_permute(0,nums, ans );
         return ans;
             
     }
