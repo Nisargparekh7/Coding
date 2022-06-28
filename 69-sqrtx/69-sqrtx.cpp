c94e36299fd6cd4d8a5e3adfeb499b034b641af6
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-06-28 11:01:41 +0530

    Time: 38 ms (6.72%), Space: 5.9 MB (24.94%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long s=0, e=x, mid,ans;
-        
-        while(s<=e){
-            mid=(s+e)/2;
-            if(mid*mid==x) return mid;
-            else if(mid*mid<x){
-                s=mid+1;
-                ans=mid;
-            }
-            else e= mid-1;
+        //
+        long long y=0;
+        while(y*y<=x){
+            y++;
         }
-        return ans;
+        return y-1;
+            
+        //time complexity(logn) space O(1);
+        // long long s=0, e=x, mid,ans;
+        // while(s<=e){
+        //     mid=(s+e)/2;
+        //     if(mid*mid==x) return mid;
+        //     else if(mid*mid<x){
+        //         s=mid+1;
+        //         ans=mid;
+        //     }
+        //     else e= mid-1;
+        // }
+        // return ans;
     }
 };
