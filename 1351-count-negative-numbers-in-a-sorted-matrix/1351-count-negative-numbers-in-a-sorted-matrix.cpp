cdcdf3995d0a54fb0f2365c48a6fd00b4ee3f084
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-06-28 12:57:24 +0530

    Time: 29 ms (27.09%), Space: 10.4 MB (85.56%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -4,26 +4,59 @@ public:
         int row= grid.size();
         int col = grid[0].size();
         int cnt=0;
-        for (int i=0;i<row; i++){
-            if(grid[i][0] < 0) cnt += grid[i].size();
-            else if( grid[i][0]>=0 && grid[i][col-1]<0){
-                int low =0,high=col-1,ans=0,mid ;
-                int h=high;
-                while(low<=high){
-                    mid=(low+high)/2;
-                    if(grid[i][mid]<0){
-                        ans=mid;
-                        high=mid-1;
-                    }
-                    else{
-                        low=mid+1;
-                    }
-                } 
-            // cout<<"cnt: "<<ans<<endl;
-               cnt= cnt+ (h-ans+1);
-            }
-        }
+         int j=0;
         
+        for(int i= row-1; i>=0;i--){
+            while(j< col &&grid[i][j]>=0){
+                j++;
+            }
+            
+            cnt+=(grid[i].size()-j);
+            //cout<< cnt <<" ";
+        }
         return cnt;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         int row= grid.size();
+//         int col = grid[0].size();
+//         int cnt=0;
+//         for (int i=0;i<row; i++){
+//             if(grid[i][0] < 0) cnt += grid[i].size();
+//             else if( grid[i][0]>=0 && grid[i][col-1]<0){
+//                 int low =0,high=col-1,ans=0,mid ;
+//                 int h=high;
+//                 while(low<=high){
+//                     mid=(low+high)/2;
+//                     if(grid[i][mid]<0){
+//                         ans=mid;
+//                         high=mid-1;
+//                     }
+//                     else{
+//                         low=mid+1;
+//                     }
+//                 } 
+//             // cout<<"cnt: "<<ans<<endl;
+//                cnt= cnt+ (h-ans+1);
+//             }
+//         }
+        
+//         return cnt;
     }
 };
