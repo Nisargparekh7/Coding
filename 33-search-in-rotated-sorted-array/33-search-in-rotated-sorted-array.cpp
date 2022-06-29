2ff8a0bf353b0e836580a8ac1cc662c955210103
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-06-30 00:30:49 +0530

    Time: 13 ms (8.67%), Space: 11.2 MB (28.98%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -10,15 +10,17 @@ public:
             mid= low+(high-low)/2;
             
             if( nums[mid]== target)return mid;
-            
+            //if left side is sorted
             if(nums[low]<=nums[mid]){
+                // then find elemrnt in that
                 if(nums[low]<= target && target<= nums[mid]){
                     high=mid-1;
                 }
                 else low=mid+1;
             }
-            
+            // else right side is sorted
             else{
+                // then find elemrnt in that
                 if(nums[mid]<= target && target<= nums[high]){
                     low=mid+1;
                 }
