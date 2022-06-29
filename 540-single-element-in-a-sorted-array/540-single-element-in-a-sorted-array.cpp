a225b4507691261dc97e8cc4e2201a86edfcc2e4
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-06-29 20:27:00 +0530

    Time: 60 ms (9.51%), Space: 22.3 MB (91.00%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -13,7 +13,7 @@ public:
                 if(mid>1 && nums[mid]!=nums[mid-1] && mid<=high &&nums[mid]!=nums[mid+1]){
                     return nums[mid];
                 }
-                else if(mid<=high &&nums[mid]!=nums[mid+1]){
+                else if(mid<=high &&nums[mid]==nums[mid-1]){
                     low=mid+1;
                 }
                 else high=mid-1;
