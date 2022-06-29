0509a1b9ec33891667615c8c208dc5e67e9ed6b7
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-06-29 20:38:35 +0530

    Time: 30 ms (83.65%), Space: 22.3 MB (54.35%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -10,19 +10,19 @@ public:
             // cout<<mid<<" ";
             if(mid%2==1){
                 
-                if(mid>1 && nums[mid]!=nums[mid-1] && mid<=high &&nums[mid]!=nums[mid+1]){
+                if(mid>=1 && nums[mid]!=nums[mid-1] && mid<=high &&nums[mid]!=nums[mid+1]){
                     return nums[mid];
                 }
-                else if(mid<=high &&nums[mid]==nums[mid-1]){
+                else if(mid>=1 &&nums[mid]==nums[mid-1]){
                     low=mid+1;
                 }
                 else high=mid-1;
             }    
             else {
-                if(mid>1 && nums[mid]!=nums[mid-1] && mid<=high &&nums[mid]!=nums[mid+1]){
+                if(mid>=1 && nums[mid]!=nums[mid-1] && mid<=high &&nums[mid]!=nums[mid+1]){
                     return nums[mid];
                 }
-                else if(mid>1 && nums[mid]==nums[mid-1]){
+                else if(mid>=1 && nums[mid]==nums[mid-1]){
                     high=mid-1;
                 }
                 else low=mid+1;
