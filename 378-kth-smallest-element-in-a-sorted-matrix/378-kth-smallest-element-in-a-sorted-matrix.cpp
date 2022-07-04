626c60eeb0b471dbcbbe185f3714b30a69eaf286
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-07-04 23:19:14 +0530

    Time: 48 ms (59.99%), Space: 13.1 MB (78.00%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int> pq;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j< matrix[0].size();j++){
-                if(pq.size()<k){
-                    pq.push(matrix[i][j]);
-                }
-                else if(matrix[i][j]>pq.top())continue;
-                else{   
-                    pq.pop();
-                    pq.push(matrix[i][j]);
-                } 
+        
+        int n= matrix.size();
+        int low=matrix[0][0]; int high=matrix[n-1][n-1];
+        
+        int mid;
+        while(low<high){
+            mid= low + (high-low)/2;
+            
+            int cnt=0;
+            for(int i=0;i<n;i++){
+                cnt+=upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+            }
+            if(cnt<k){
+                low=mid+1;
+            }
+            else{
+                high=mid;
             }
         }
-        return pq.top();
+        return low;
+        //priority_queue<int> pq;
+        // for(int i=0;i<matrix.size();i++){
+        //     for(int j=0;j< matrix[0].size();j++){
+        //         if(pq.size()<k){
+        //             pq.push(matrix[i][j]);
+        //         }
+        //         else if(matrix[i][j]>pq.top())continue;
+        //         else{   
+        //             pq.pop();
+        //             pq.push(matrix[i][j]);
+        //         } 
+        //     }
+        // }
+        // return pq.top();
     }
 };
