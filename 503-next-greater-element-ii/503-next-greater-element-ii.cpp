b21667a81433a14beac3e871ceabcb4ae43927b9
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-07-12 11:15:41 +0530

    Time: 59 ms (43.01%), Space: 23.9 MB (62.74%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -1,25 +1,60 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        //map<int,int> m;
+//        
+        map<int,int> m;
         stack<int> st;
         int n= nums.size();
+        vector<int> ans(n);
         
-        vector<int> ans(n,-1);
         for(int i=2*n-1;i>=0;i--){
             
             while(!st.empty() && st.top()<=nums[i%n]){
                 st.pop();
             }
-            if(i<n){
-    
-                if(!st.empty()){
-                    ans[i]=st.top();
-                }
+            
+            if(st.empty()) {
+                ans[i%n] = -1;
+            }
+            
+            else{
+                ans[i%n] =st.top();
             }
             st.push(nums[i%n]);
         }
-    
+//         vector<int> ans;
+        
+//         for(auto i: nums){
+//             ans.push_back(m[i]);
+//         }
         return ans;
+        
+        
+        
+        //map<int,int> m;
+//         stack<int> st;
+//         int n= nums.size();
+        
+//         for(int i=0;i<=(2*n)-1;i++){
+            
+//             while(!st.empty() && st.top()<=nums[i%n]){
+//                 st.pop();
+//             }
+            
+//             if(st.empty()) {
+//                 m[nums[i%n]]=-1;
+//             }
+            
+//             else{
+//                 m[nums[i%n]]=st.top();
+//             }
+//             st.push(nums[i%n]);
+//         }
+//         vector<int> ans;
+        
+//         for(auto i: nums){
+//             ans.push_back(m[i]);
+//         }
+//         return ans;
     }
 };
