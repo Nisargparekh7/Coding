c72c61e7b82dbd84840583600f0fd315455e7e7a
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-07-18 13:26:52 +0530

    Time: 62 ms (7.66%), Space: 28.6 MB (99.87%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -13,16 +13,14 @@ class Solution {
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         if(!root)return 0 ;
-         if(root->val == subRoot->val){    
-            if(check(root,subRoot)){
-            return true;
-        }
-         }
+            // if(root->val == subRoot->val){    
+                if(check(root,subRoot)){
+                    return true;
+                }
+            // }
         
        return isSubtree(root->left,subRoot) ||isSubtree(root->right,subRoot);
         
-
-        //return 0;
     }
     
     bool check(TreeNode* root, TreeNode* subRoot){
