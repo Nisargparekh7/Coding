a48421aabc70795133bb13b166eca450aa744a44
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-08-01 23:34:54 +0530

    Time: 7 ms (69.59%), Space: 13.8 MB (48.38%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -21,5 +21,43 @@ public:
         root->right = bstFromPreorder(preorder, max_val);
         
         return root;
+        
+        
+        
+        
+//         if (preorder.size() == 0){  // If vector is empty, return NULL
+//             return NULL;
+//         }
+        
+//         TreeNode* root = new TreeNode(preorder[0]); 
+//         for (int i = 1; i < preorder.size(); i++){  // Traversing the vector
+//             bool done = false;  // Marker to indicate when the vector value has been added to the tree
+//             TreeNode* current = root;   // Current is used to traverse the tree we're creating
+            
+//             while (done == false){
+//                 if (preorder[i] < current->val){    
+//                     if (current->left == NULL){ 
+//                         current->left = new TreeNode(preorder[i]);
+//                         done = true;    
+//                     }
+//                     else{   
+//                         current = current->left;
+//                     }
+//                 }
+//                 else{   
+//                     if (current->right == NULL){ // Add the vector value to tree if spot is available
+//                         current->right = new TreeNode(preorder[i]);
+//                         done = true; // Vector value added, therefore break out of while loop
+//                     }
+//                     else{
+//                         current = current->right;   // No spot available, keep traversing tree
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return root;
+        
+        
     }
 };
