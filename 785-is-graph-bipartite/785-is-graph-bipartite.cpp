8217b76365798ce77cb3aafacbc2cec46393a0eb
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-08-16 19:16:02 +0530

    Time: 78 ms (7.32%), Space: 15.4 MB (13.14%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -4,13 +4,27 @@ public:
        vector<int> color(graph.size(),-1);
         for(int i=0;i<graph.size();i++){
             if(color[i]==-1){
-                if(!checkbfs(i,graph,color))return false;
+                if(!check_BFS(i,graph,color))return false;
             }
         }
         return true;
     }
     
-     bool checkbfs(int src, vector<vector<int>>& graph , vector<int> color){
+    
+        //using DFS
+       bool check_DFS(int node, vector<vector<int>>& graph , vector<int> color){
+            for(auto it: graph[node]){
+                if(color[it]==-1){
+                    color[it]=1-color[node];
+                    if(!check_DFS(it,graph,color))return false;
+                }
+                else if(color[it]==color[node])return false;
+            }
+        return true;
+        } 
+    
+    //using BFS
+     bool check_BFS(int src, vector<vector<int>>& graph , vector<int> color){
         
         queue<int>q;
         q.push(src);
@@ -25,9 +39,8 @@ public:
                 if(color[it]==-1){
                     color[it]=1-color[node];
                    q.push(it);
-                  //  if(!checkbfs(it,graph,color))return false;
                 }
-                //
+                //else if does color matches with node, return false
                 else if(color[it]==color[node])return false;
             }
         }
@@ -39,29 +52,5 @@ public:
     
     
     
-    
-    
-    bool checkdfs(int node, vector<vector<int>>& graph , vector<int> color){
-        
-//         queue<int>q;
-//         q.push(src);
-//         //intialize color with one, and later we use 1-color[src];
-//         color[src]=1;
-        
-        // while(!q.empty()){
-        //     int node= q.front();
-        //     q.pop();
-            for(auto it: graph[node]){
-                //if adj node is -1, color it and push it into queue
-                if(color[it]==-1){
-                    color[it]=1-color[node];
-                  //  q.push(it);
-                    if(!checkdfs(it,graph,color))return false;
-                }
-                //
-                else if(color[it]==color[node])return false;
-            }
-        return true;
-        }
-    // }
+
 };
