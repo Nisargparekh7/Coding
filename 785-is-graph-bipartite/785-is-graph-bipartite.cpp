63fabaf5e0772378a974f7c0fd1c0de14ab2f337
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-08-16 19:13:05 +0530

    Time: 83 ms (6.69%), Space: 15.3 MB (13.87%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -4,30 +4,64 @@ public:
        vector<int> color(graph.size(),-1);
         for(int i=0;i<graph.size();i++){
             if(color[i]==-1){
-                if(!check(i,graph,color))return false;
+                if(!checkbfs(i,graph,color))return false;
             }
         }
         return true;
     }
     
-    
-    bool check(int src, vector<vector<int>>& graph , vector<int> color){
+     bool checkbfs(int src, vector<vector<int>>& graph , vector<int> color){
         
         queue<int>q;
         q.push(src);
+        //intialize color with one, and later we use 1-color[src];
         color[src]=1;
         
         while(!q.empty()){
             int node= q.front();
             q.pop();
             for(auto it: graph[node]){
+                //if adj node is -1, color it and push it into queue
                 if(color[it]==-1){
                     color[it]=1-color[node];
-                    q.push(it);
+                   q.push(it);
+                  //  if(!checkbfs(it,graph,color))return false;
                 }
+                //
                 else if(color[it]==color[node])return false;
             }
         }
         return true;
-    }
+     }
+    
+    
+    
+    
+    
+    
+    
+    
+    bool checkdfs(int node, vector<vector<int>>& graph , vector<int> color){
+        
+//         queue<int>q;
+//         q.push(src);
+//         //intialize color with one, and later we use 1-color[src];
+//         color[src]=1;
+        
+        // while(!q.empty()){
+        //     int node= q.front();
+        //     q.pop();
+            for(auto it: graph[node]){
+                //if adj node is -1, color it and push it into queue
+                if(color[it]==-1){
+                    color[it]=1-color[node];
+                  //  q.push(it);
+                    if(!checkdfs(it,graph,color))return false;
+                }
+                //
+                else if(color[it]==color[node])return false;
+            }
+        return true;
+        }
+    // }
 };
