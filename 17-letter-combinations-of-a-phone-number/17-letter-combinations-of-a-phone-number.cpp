a58611f79a0d6c0ec365a9d714e5c675fa761f40
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-08-22 13:03:58 +0530

    Time: 3 ms (46.37%), Space: 6.5 MB (55.12%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -11,6 +11,8 @@ public:
         return ans;
 
     }
+    
+    //function solve
     void solve(string digits, int index, string output, vector<string> &ans, string mapping[]){
         
         //base case
@@ -18,13 +20,18 @@ public:
             ans.push_back(output);
             return;
         }
-        
-         int number= digits[index] - '0';
+        // convert number to string
+        int number= digits[index] - '0';
+        // find the mapping of the string. e.g 2-> "abc"
         string s= mapping[number];
         
+        // running the  lopp for all the character. e.g a,b,c in "abc"
         for(int i=0; i<s.length();i++){
+            //taken the element
             output.push_back(s[i]);
+            //recursive call
             solve(digits, index+1, output, ans,mapping);
+            //not taken the element and again for loop will run.
             output.pop_back();
         }
         
