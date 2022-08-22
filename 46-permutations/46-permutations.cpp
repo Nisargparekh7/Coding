d81e17edc68291472d727ecf8a7345a05b481fc1
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-08-22 13:25:57 +0530

    Time: 3 ms (82.35%), Space: 7.6 MB (91.69%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     void recursion_permute(int index,vector<int>&nums,vector<vector<int>>&ans){
+        //base case
         if(index==nums.size()){
             ans.push_back(nums);
             return;
         }
+        
+        //swaping every index with 1st position
         for(int i=index;i<nums.size();i++){
+            //swapping
             swap(nums[index],nums[i]);
+            
+            //recusion call
             recursion_permute(index+1,nums,ans);
+            
+            //backtrack- as for 2nd time when loop runs we can swap with orinigal strings.
             swap(nums[index],nums[i]);
             }
         
@@ -15,10 +23,8 @@ public:
     
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        // vector<int> temp;
-        // int freq[nums.size()];
-        // for(int i=0;i<nums.size();i++) freq[i]=0;
-        recursion_permute(0,nums, ans );
+
+        recursion_permute(0,nums, ans);
         return ans;
             
     }
