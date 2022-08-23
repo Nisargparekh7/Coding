b83e0220da21d024033095e45b4f49da1f511da8
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-08-23 19:35:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (57.31%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
-    //using memoization
+    
+    //using tabulation with space optimization
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return climb(n,dp);
+        int a=1,b=1;
+        int c=1;
+        for(int i=2;i<=n;i++){
+            c= a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+        
     }
-    int climb(int n, vector<int> &dp){
-        if(n==0 || n==1)return 1;
-        if(dp[n]!= -1) return dp[n];
-        return dp[n]=climb(n-1,dp)+ climb(n-2,dp);
-    }
+    
+    
+    
+    
+    
+    //using memoization
+    // int climbStairs(int n) {
+    //     vector<int> dp(n+1,-1);
+    //     return climb(n,dp);
+    // }
+    // int climb(int n, vector<int> &dp){
+    //     if(n==0 || n==1)return 1;
+    //     if(dp[n]!= -1) return dp[n];
+    //     return dp[n]=climb(n-1,dp)+ climb(n-2,dp);
+    // }
 };
