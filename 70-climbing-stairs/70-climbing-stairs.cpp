093aec7cf99ed63eb771b76e0f4ca158ca2304fc
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-08-23 19:36:58 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.63%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -3,8 +3,11 @@ public:
     
     //using tabulation with space optimization
     int climbStairs(int n) {
+        // here a=1 & b=1 (unlike of fibonacci number);
         int a=1,b=1;
+        //if i=1 return 1. i.e c is intialized with 1;
         int c=1;
+        
         for(int i=2;i<=n;i++){
             c= a+b;
             a=b;
