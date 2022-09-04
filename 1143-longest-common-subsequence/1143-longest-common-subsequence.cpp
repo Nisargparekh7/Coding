c6ac634abbf269eeb01e5739de7042d12086c3a7
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-09-04 17:35:34 +0530

    Time: 50 ms (28.90%), Space: 12.9 MB (56.64%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,18 +3,43 @@ public:
     int longestCommonSubsequence(string s, string t) {
         int n1=s.length();
         int n2=t.length();
-        vector<vector<int>> dp(n1, vector<int>(n2,-1));
-        return f(n1-1, n2-1, s,t,dp);
-    }
-    int f(int ind1,int ind2,string &s, string &t,vector<vector<int>> &dp){
-    //basecase
-    if(ind1 <0 || ind2<0){
-        return 0;
-    }
-    if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
-    // all possible way 
-    if(s[ind1]==t[ind2])return dp[ind1][ind2]= 1+ f(ind1-1, ind2-1,s,t,dp);
-    return dp[ind1][ind2]= max(f(ind1, ind2-1,s,t,dp),f(ind1-1, ind2,s,t,dp));
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1,0));
+
+        //base case- using shifiting index\
+        for(int ind2=0;ind2<=n2;ind2++)dp[0][ind2]=0;
+        for(int ind1=0;ind1<=n1;ind1++)dp[ind1][0]=0;
+
+         for(int ind1=1;ind1<=n1;ind1++){
+           for(int ind2=1;ind2<=n2;ind2++){
+               if(s[ind1-1]==t[ind2-1]) dp[ind1][ind2]= 1+ dp[ind1-1][ind2-1];
+
+               else dp[ind1][ind2]= max(dp[ind1][ind2-1],dp[ind1-1][ind2]);
+
+             }
+         }
+        //code for printing LCS
+        int len=dp[n1][n2];
+        string ans="";
+        for(int i=0;i<len;i++)ans+='$';
+        
+        int index=len-1;
+        int i=n1, j=n2;
+        
+        while(i>0 && j>0){
+            if(s[i-1]==t[j-1]){
+                ans[index]=s[i-1];
+                index--;
+                i--,j--;
+            }
+            else if(dp[i-1][j]> dp[i][j-1])i--;
+            else j--;
+        }
+        cout<<ans;
+        //till here 
+        
+        
+        return dp[n1][n2];
+
 }
     
 };
