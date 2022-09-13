bcf6c19bbf6b7b0fda18f341343cac3462c68185
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-09-13 13:02:53 +0530

    Time: 12 ms (82.59%), Space: 9.2 MB (21.05%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int f(int i,int j, string &s1, string &s2, vector<vector<int>> &dp){
-        //base case
-        if(i<0)return j+1; //j+1 steps for inserstion
-        if(j<0)return i+1; // i+1 steps for delection
+        //base case -- 1 base indexing.  Refer previous submission for 0 base indexing
+        if(i==0)return j; //j steps for inserstion
+        if(j==0)return i; // i steps for delection
         
         //explore all possible case
         if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i]==s2[j]) return dp[i][j]= f(i-1,j-1,s1,s2,dp);
+          
+        if(s1[i-1]==s2[j-1]) return dp[i][j]= f(i-1,j-1,s1,s2,dp);
         return dp[i][j]= 1+ min( f(i-1,j,s1,s2,dp), min(f(i,j-1,s1,s2,dp), f(i-1,j-1,s1,s2,dp)) );
     }
     
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m= word2.size();
-        vector<vector<int>> dp(n, vector<int>(m,-1));
-        return f(n-1, m-1, word1,word2,dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        return f(n, m, word1,word2,dp);
     }
 };
