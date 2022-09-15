c144df8a02e4ae7788fa4ce305513641904c734c
Nisargparekh7 <48588549+Nisargparekh7@users.noreply.github.com>
2022-09-15 09:22:07 +0530

    Time: 11 ms (88.67%), Space: 10.4 MB (87.90%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,47 +1,19 @@
 class Solution {
 public:
-//     int f(int ind, int prev_ind, vector<int>&nums,int n,vector<vector<int>>&dp){
-//         //base case
-//         if(ind==n)return 0;
-        
-//         if(dp[ind][prev_ind+1]!=-1)return dp[ind][prev_ind+1];
-//         //explore all possible
-//         int len= 0+f(ind+1, prev_ind, nums,n,dp);
-    
-//         if(prev_ind==-1 || nums[ind]> nums[prev_ind]){
-//             len=max(len, 1+f(ind+1, ind, nums,n,dp));
-//         }
-//         return dp[ind][prev_ind+1]=len;
-        
-//         }
-    
+//     using binary search
     
     int lengthOfLIS(vector<int>& nums) {
-       
-        int n=nums.size();
-        vector<int> dp(n,1), hash(n);
-        int maxi=1;
-        
-        for(int i=0;i<n;i++){
-            hash[i]=i;
-            
-            for(int prev=0;prev<i;prev++){
-                
-                if(nums[prev]<nums[i] && 1+dp[prev]>dp[i]){
-                    dp[i]=1+dp[prev];
-                    hash[i]=prev;
-                }
-                
+       vector<int> temp;
+        temp.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>temp.back()) temp.push_back(nums[i]);
+            else{
+                int ind=lower_bound(temp.begin(), temp.end(), nums[i])-temp.begin();
+                temp[ind]=nums[i];
             }
-            
-            if(dp[i]>maxi){
-            maxi=dp[i];
-            }
-      
+                
         }
-        
-       return maxi;
-        
+        return temp.size();
     }
     
     
